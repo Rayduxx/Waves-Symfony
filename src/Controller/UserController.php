73dec749b9f8b685e2a1efae3930c6385a0904be
Rayduxx <moussa.ala@esprit.tn>
2024-04-17 17:56:27 +0100

    updates

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -147,4 +147,9 @@ class UserController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+    #[Route('/users', name: 'app_user_list')]
+    public function UsersList()
+    {
+        return $this->render('user/listusers.html.twig');
+    }
 }
