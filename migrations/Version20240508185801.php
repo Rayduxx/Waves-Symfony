8a817e7cb0fc7442ed2a2a3164b9cc64fae74888
Kais Berrehouma <94445493+Meey0h@users.noreply.github.com>
2024-05-08 20:46:00 +0100

    mise en forme

--- migrations/Version20240508185801.php
+++ migrations/Version20240508185801.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240508045932 extends AbstractMigration
+final class Version20240508185801 extends AbstractMigration
 {
     public function getDescription(): string
     {
